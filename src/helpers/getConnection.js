37ea75458bcdd55384c40e8edd4e3189479ebae1
ayush_1197 <ayush.gupta.1197@gmail.com>
2019-09-19 01:23:24 +0530

    changes

--- src/helpers/getConnection.js
+++ src/helpers/getConnection.js
@@ -2,6 +2,13 @@ import mongo from 'mongojs'
 import dbConfig from '../config/dbConfig'
 
 const db = mongo(dbConfig.url, dbConfig.collections)
-console.log("DB Connected!")
+
+db.on('error', function (err) {
+    console.log('database error', err)
+})
+ 
+db.on('connect', function () {
+    console.log('database connected')
+})
 
 export default db
