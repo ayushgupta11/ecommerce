25634628c1b83831c6c749f354feb55ebdc4a892
ayush_1197 <ayush.gupta.1197@gmail.com>
2019-09-28 20:02:12 +0530

    changes

--- src/controllers/product/getById.controller.js
+++ src/controllers/product/getById.controller.js
@@ -3,15 +3,20 @@ import mongojs from 'mongojs'
 
 export default (db) => {
     return (request, response) => {
-        db.products.findOne({
-            '_id': mongojs.ObjectId(request.body.data._id)
-        }, (err, doc) => {
-            if (err) {
-                internalServerError(response, err)
-            }
-            else {
-                success(response, doc)
-            }
-        })
+        if(request.body.data){
+            db.products.findOne({
+                '_id': mongojs.ObjectId(request.body.data._id)
+            }, (err, doc) => {
+                if (err) {
+                    internalServerError(response, err)
+                }
+                else {
+                    success(response, doc)
+                }
+            })
+        }
+        else{
+            badRequest(response)
+        }
     }
 }
